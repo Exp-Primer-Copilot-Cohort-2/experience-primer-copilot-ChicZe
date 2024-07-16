88cd4ba9a170d14b771668ee97f4e251b1025b1d
ciusca <96741973+ChicZe@users.noreply.github.com>
2024-07-16 13:42:37 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,58 +1,14 @@
-// create web server
-// create a comment
-// read all comments
-// read a comment
-// update a comment
-// delete a comment
-
-const express = require("express");
+// Create web server
+const express = require('express');
 const app = express();
-app.use(express.json());
+const port = 3000;
 
-let comments = [];
-
-app.get("/comments", (req, res) => {
-  res.send(comments);
+// Create a route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
-app.get("/comments/:id", (req, res) => {
-  const comment = comments.find((comment) => comment.id === parseInt(req.params.id));
-  if (!comment) {
-    return res.status(404).send("The comment with the given ID was not found");
-  }
-  res.send(comment);
-});
-
-app.post("/comments", (req, res) => {
-  const comment = {
-    id: comments.length + 1,
-    name: req.body.name,
-    comment: req.body.comment,
-  };
-  comments.push(comment);
-  res.send(comment);
-});
-
-app.put("/comments/:id", (req, res) => {
-  const comment = comments.find((comment) => comment.id === parseInt(req.params.id));
-  if (!comment) {
-    return res.status(404).send("The comment with the given ID was not found");
-  }
-  comment.name = req.body.name;
-  comment.comment = req.body.comment;
-  res.send(comment);
-});
-
-app.delete("/comments/:id", (req, res) => {
-  const comment = comments.find((comment) => comment.id === parseInt(req.params.id));
-  if (!comment) {
-    return res.status(404).send("The comment with the given ID was not found");
-  }
-  const index = comments.indexOf(comment);
-  comments.splice(index, 1);
-  res.send(comment);
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// Start the server
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
